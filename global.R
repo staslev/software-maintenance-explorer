b858d769b1b493798a80577017c9005bc8ba79c0
Stas Levin <staslevin@gmail.com>
2017-07-15 19:21:59 +0300

    Replaced read.table with read.csv, the former was misbehaving

--- global.R
+++ global.R
@@ -1,3 +1,3 @@
 library(data.table)
-rawData = read.table(paste0(getwd(),"/","data-vis-final-prj.csv"), sep = "#", header = TRUE, comment.char = "")
+rawData = read.csv(paste0(getwd(),"/","data-vis-final-prj.csv"), sep = "#", header = TRUE, comment.char = '')
 setDT(rawData)
